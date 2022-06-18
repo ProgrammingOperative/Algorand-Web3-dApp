8637c89d17150d3c4e8383e0a9335b4f23c59105
waCira <wachura11t@gmail.com>
2022-06-19 01:40:27 +0300

    Add frontend menu

--- front-end/manage_nft.py
+++ front-end/manage_nft.py
@@ -5,6 +5,18 @@ app = AdminApp(use_fastapi=True)
 def on_submit(form_data):
     print(form_data)
 
+
+
+app.set_menu(
+    [
+        MenuItem('Home', '/', icon="dashboard", children=[
+            MenuItem('10 Academy Staff', '/admin', icon="setting"),
+            MenuItem('Trainee', '/admin', icon="setting")
+        ]),
+        MenuItem('About', '/about', icon="info-circle")
+    ]
+  )
+
 @app.page('/', 'Form')
 def form_page():
     return [
